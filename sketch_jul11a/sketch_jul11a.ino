a5891527dee98cc4c48346116c8b7032b9392da3
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-07-13 11:30:03 -0400

    Working on servos

--- sketch_jul11a/sketch_jul11a.ino
+++ sketch_jul11a/sketch_jul11a.ino
@@ -1,3 +1,9 @@
+#include <Servo.h>;
+
+Servo clawL;
+Servo clawR;
+int pos =0;
+
 void setup() {
   // put your setup code here, to run once:
   
@@ -9,6 +15,11 @@ void setup() {
   pinMode(8,OUTPUT); //B Brake
   analogWrite(11,0); //B speed
 
+  clawL.attach(7);
+  clawR.attach(5); //this is the pin that is being used
+  clawL.write(0);
+  clawR.write(0);
+  
 
   Serial.begin(9600); //bit rate
   Serial.println("Ready to go!");
@@ -19,36 +30,37 @@ void loop() {
 
   if(Serial.available()){
     char key = Serial.read();
-    Serial.println(key);
-  
+    Serial.println(key); 
 
     if(key=='w'){
       moveA(200,false);
       moveB(200,true);//they are different because the motors are flipped so if they were the same it would turn
-    }
- 
+    } 
     else if(key =='a'){
       moveA(200,false);
       moveB(200,false);
     }
     else if(key =='d'){
       moveB(200,true);
-      
+      moveA(200,true);      
     }
     else if(key == 's'){
       moveA(200,true);
-
       moveB(200,false);
-         }
-
+    }
     else if(key == 'k'){
       moveA(0,true);
       moveB(0,false);
     }
+    else if(key=='i'){
+      clawL.write(160); //this is a value from 0-180 the angel of rotation
+      clawR.write(0); //this is a value from 0-180 the angel of rotation
+    }
+    else if(key=='j'){
+      clawL.write(120); //this is a value from 0-180 the angel of rotation
+      clawR.write(10);
+    }
   }
-
-  
-
 }
 
 void moveA(int speed, boolean dir) {
